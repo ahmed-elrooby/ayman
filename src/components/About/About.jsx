a2092c506fb8d54e42cc94ece12ff204511204ae
ahmed-elrooby <ahmdalrwby24@gmail.com>
2026-01-31 21:00:53 +0200

    ll

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -5,8 +5,9 @@ import about from "../../Images/about.json";
 import ProfisSkills from "../ProfisSkills/ProfisSkills";
 import Services from "../Services/Services";
 import { motion } from "framer-motion";
-import Lottie from "react-lottie-player";
+import dynamic from "next/dynamic";
 
+const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
 const About = () => {
   return (
     <>
@@ -20,8 +21,7 @@ const About = () => {
           >
             <h1 className="md:text-[30px] text-[20px] flex items-center capitalize font-bold">
               Here's a quick introduction about me
-            
-               <Lottie
+              <Lottie
                 animationData={about}
                 className="md:w-[100px] w-[50px] h-[50px] md:h-[100px] max-w-full"
                 loop={true}
@@ -30,11 +30,36 @@ const About = () => {
               />
             </h1>
             <p className="text-[21px] leading-[2] capitalize">
-              "Hi, I'm <span className="text-[--main-color] font-bold">Mohamed Ayman</span>, a professional Graphic Designer and Front-End Developer. I specialize in HTML, CSS, Tailwind CSS, and UI/UX design. I create visually appealing and user-friendly websites. You can contact me at <span className="font-bold">01140293916</span> or via email: <span className="font-bold">ssmohamedayman643@gmail.com</span>. Follow me on Instagram: <a href="https://www.instagram.com/moh_amed_ayman_223?igsh=MXJnMnZta21ocDlsZw==" target="_blank" className="text-[--main-color] underline">Instagram</a> and Facebook: <a href="https://www.facebook.com/share/1BWBovbvbW/" target="_blank" className="text-[--main-color] underline">Facebook</a>."
+              "Hi, I'm{" "}
+              <span className="text-[--main-color] font-bold">
+                Mohamed Ayman
+              </span>
+              , a professional Graphic Designer and Front-End Developer. I
+              specialize in HTML, CSS, Tailwind CSS, and UI/UX design. I create
+              visually appealing and user-friendly websites. You can contact me
+              at <span className="font-bold">01140293916</span> or via email:{" "}
+              <span className="font-bold">ssmohamedayman643@gmail.com</span>.
+              Follow me on Instagram:{" "}
+              <a
+                href="https://www.instagram.com/moh_amed_ayman_223?igsh=MXJnMnZta21ocDlsZw=="
+                target="_blank"
+                className="text-[--main-color] underline"
+              >
+                Instagram
+              </a>{" "}
+              and Facebook:{" "}
+              <a
+                href="https://www.facebook.com/share/1BWBovbvbW/"
+                target="_blank"
+                className="text-[--main-color] underline"
+              >
+                Facebook
+              </a>
+              ."
             </p>
           </motion.div>
           <motion.div
-          className="relative"
+            className="relative"
             initial={{ scale: 0.7, opacity: 0.5 }}
             transition={{ duration: 1.3, type: "tween" }}
             whileInView={{ scale: 1, opacity: 1 }}
@@ -43,12 +68,14 @@ const About = () => {
               animationData={aboutLottie}
               className="max-w-full w-[350px] md:w-[400px]"
               loop={true}
-               play
+              play
             />
           </motion.div>
         </div>
         <p className="m-auto text-[25px] text-gray-600 dark:text-gray-300 capitalize w-full text-center mt-[25px]">
-          👉 Creativity is like code. When you have to explain it, it’s bad. I design clean, simple, and user-friendly interfaces that speak for themselves.
+          👉 Creativity is like code. When you have to explain it, it’s bad. I
+          design clean, simple, and user-friendly interfaces that speak for
+          themselves.
         </p>
         <ProfisSkills />
       </div>
--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import arrow from "../../Images/arrow.json"
-import { motion } from 'framer-motion';
-import Lottie from 'react-lottie-player';
+import React from "react";
+import arrow from "../../Images/arrow.json";
+import { motion } from "framer-motion";
+import dynamic from "next/dynamic";
+const Lottie = dynamic(() => import("react-lottie-player"), { ssr: false });
+
 import { FaPalette, FaLayerGroup, FaTabletAlt } from "react-icons/fa";
 
 const Services = () => {
   return (
     <>
-      <div className='m-auto mt-[50px]'>
+      <div className="m-auto mt-[50px]">
         <span className="block w-[200px] m-auto h-[3px] bg-[--main-color]"></span>
 
         <motion.div
@@ -16,42 +18,54 @@ const Services = () => {
           whileInView={{ scale: 1, opacity: 1 }}
           className="flex flex-col items-center justify-center gap-[40px]"
         >
-          <h1 className='font-bold text-[25px] md:text-[35px] justify-center text-[--main-color] flex text-center items-center'>
+          <h1 className="font-bold text-[25px] md:text-[35px] justify-center text-[--main-color] flex text-center items-center">
             What I Can Do For You
-            <Lottie className='w-[100px] h-[100px]' animationData={arrow} play loop={true} />
+            <Lottie
+              className="w-[100px] h-[100px]"
+              animationData={arrow}
+              play
+              loop={true}
+            />
           </h1>
 
-          <div className='flex items-center flex-wrap justify-center gap-[15px]'>
-
+          <div className="flex items-center flex-wrap justify-center gap-[15px]">
             {/* Logo Design */}
             <div className="flex rounded-lg hover:shadow-lg transition-all group hover:bg-[--main-color] flex-col justify-center text-center items-center h-[300px] w-[350px] gap-1 border-2 border-[--main-color]">
-              <h1 className='font-bold text-[--main-color] text-[30px] group-hover:text-white'>
+              <h1 className="font-bold text-[--main-color] text-[30px] group-hover:text-white">
                 Logo Design 🎨
               </h1>
-              <FaPalette className="text-[--main-color] group-hover:text-white transition-all" size={150} />
+              <FaPalette
+                className="text-[--main-color] group-hover:text-white transition-all"
+                size={150}
+              />
             </div>
 
             {/* Branding & Social Media */}
             <div className="flex rounded-lg hover:shadow-lg transition-all group hover:bg-[--main-color] flex-col justify-center text-center items-center h-[300px] w-[350px] gap-1 border-2 border-[--main-color]">
-              <h1 className='font-bold text-[--main-color] text-[30px] group-hover:text-white'>
+              <h1 className="font-bold text-[--main-color] text-[30px] group-hover:text-white">
                 Branding & Social Media 🖼️
               </h1>
-              <FaLayerGroup className="text-[--main-color] group-hover:text-white transition-all" size={150} />
+              <FaLayerGroup
+                className="text-[--main-color] group-hover:text-white transition-all"
+                size={150}
+              />
             </div>
 
             {/* UI/UX Design */}
             <div className="flex rounded-lg hover:shadow-lg transition-all group hover:bg-[--main-color] flex-col justify-center text-center items-center h-[300px] w-[350px] gap-1 border-2 border-[--main-color]">
-              <h1 className='font-bold text-[--main-color] text-[30px] group-hover:text-white'>
+              <h1 className="font-bold text-[--main-color] text-[30px] group-hover:text-white">
                 UI/UX Design 📱✨
               </h1>
-              <FaTabletAlt className="text-[--main-color] group-hover:text-white transition-all" size={150} />
+              <FaTabletAlt
+                className="text-[--main-color] group-hover:text-white transition-all"
+                size={150}
+              />
             </div>
-
           </div>
         </motion.div>
       </div>
     </>
   );
-}
+};
 
 export default Services;
